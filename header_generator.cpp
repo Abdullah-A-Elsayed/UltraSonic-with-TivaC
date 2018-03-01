8eca2658babac31f6e6d969cdc8a3b3accda14ce
Aladdin <alaa2005721@gmail.com>
2018-03-01 13:09:58 +0200

    adding ports base addresses

--- header_generator.cpp
+++ header_generator.cpp
@@ -6,23 +6,32 @@
 #define define "#define P"
 #define type   "(volatile uint32_t *)0x"
 
-using namespace std; 
+using namespace std;
 
 int main()
 {
 	ofstream x ;
-	char port='A';
-	int address = 0x40004;
 
 	x.open("bit_specific_addressing.h");
+	x<<hex;
 
-	for(int i=0 ; i<6 ; i++,port++)
+	char port='A';
+	x<<"//---------------------PORTS_BASE_ADDRESSES---------------------//"<<endl;
+	for(int address=0x40004,i=0 ; i<6 ; i++,port++)
+	{
+		x<<define<<port<<" "<<type<<address<<"000"<<endl;
+		address=(i<3)?++address:(i<4)?0x40024:0x40025;
+	}
+	x<<endl<<endl<<endl;
+
+	port='A';
+	for(int address = 0x40004,i=0; i<6 ; i++,port++)
 	{
 		x<<"//---------------------PORT"<<port<<"---------------------//"<<endl;
 		for(int pinaddress=4,j=0 ; j<8 ; j++,pinaddress*=2)
 		{
 			if(j<2)
-				x<<define<<port<<j<<" "<<type<<hex<<address<<"00"<<pinaddress<<endl;
+				x<<define<<port<<j<<" "<<type<<address<<"00"<<pinaddress<<endl;
 			else if(j<6)
 				x<<define<<port<<j<<" "<<type<<address<<"0"<<pinaddress<<endl;
 			else

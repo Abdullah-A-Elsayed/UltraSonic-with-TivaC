9ad2d7eb883e30232dbdb9f1ae3ffc497121ec95
Aladdin <alaa2005721@gmail.com>
2018-03-01 12:50:56 +0200

    revised copy

--- portMode.c
+++ portMode.c
@@ -2,8 +2,8 @@
 #include "C:\Keil\EE319Kware\inc\tm4c123gh6pm.h"
 
 
-#define fn1(X,NUM)                               \
-{                                                \
+#define fn1(X,NUM)                                 \
+{                                                  \
 	SYSCTL_RCGCGPIO_R |= NUM;                      \
 	while ((SYSCTL_PRGPIO_R & NUM)==0) {};         \
 	GPIO_PORT ## X ## _LOCK_R  = 0x4C4F434B ;      \
@@ -11,10 +11,10 @@
 	GPIO_PORT ## X ## _DIR_R   = mode;             \
 	GPIO_PORT ## X ## _DEN_R   = 0xFF;             \
 	break;                                         \
-}                                                \
+}                                                  \
 
-#define fn2(X,NUM)                               \
-{                                                \
+#define fn2(X,NUM)                                 \
+{                                                  \
 	SYSCTL_RCGCGPIO_R |= NUM;                      \
 	while ((SYSCTL_PRGPIO_R & NUM)==0) {};         \
 	GPIO_PORT ## X ## _LOCK_R  = 0x4C4F434B ;      \
@@ -24,8 +24,8 @@
 	break;                                         \
 }
 
-#define fn3(X,NUM)                               \
-{                                                \
+#define fn3(X,NUM)                                 \
+{                                                  \
 	SYSCTL_RCGCGPIO_R |= NUM;                      \
 	while ((SYSCTL_PRGPIO_R & NUM)==0) {};         \
 	GPIO_PORT ## X ## _LOCK_R  = 0x4C4F434B ;      \
@@ -35,19 +35,19 @@
 	break;                                         \
 }
 
-#define create(NAME,X)                                \
-void NAME(volatile uint32_t * px, uint8_t mode)       \
-{                                                     \
-	switch((uint32_t)px)                                \
-	{                                                   \
-		case 0x40004000 : fn ## X ##(A , 0x1)             \
-		case 0x40005000 : fn ## X ##(B , 0x2)             \
-		case 0x40006000 : fn ## X ##(C , 0x4)             \
-		case 0x40007000 : fn ## X ##(D , 0x8)             \
-		case 0x40024000 : fn ## X ##(E , 0x10)            \
-		case 0x40025000 : fn ## X ##(F , 0x20)            \
-	}                                                   \
-}                                                     \
+#define create(NAME,X)                             \
+void NAME(volatile uint32_t * px, uint8_t mode)    \
+{                                                  \
+	switch((uint32_t)px)                           \
+	{                                              \
+		case 0x40004000 : fn ## X(A , 0x1)         \
+		case 0x40005000 : fn ## X(B , 0x2)         \
+		case 0x40006000 : fn ## X(C , 0x4)         \
+		case 0x40007000 : fn ## X(D , 0x8)         \
+		case 0x40024000 : fn ## X(E , 0x10)        \
+		case 0x40025000 : fn ## X(F , 0x20)        \
+	}                                              \
+}                                                  \
 
 create(portMode,1)
 create(portSetOutputs,2)

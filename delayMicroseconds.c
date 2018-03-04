0896385e63672bfd49737d8ee75af14cb6ba9bfb
omarashraf10 <omarashraf465@yahoo.com>
2018-03-04 11:28:56 +0200

    adding delay function

---
+++ delayMicroseconds.c
@@ -0,0 +1,16 @@
+#include "stdint.h"
+#include "E:\tiva\tm4c123gh6pm.h"
+
+void delayMicroseconds( int  t)       
+{
+	int i;
+	for(i=0; i<t ;i++)
+	{
+	NVIC_ST_CTRL_R    = 0;
+	NVIC_ST_RELOAD_R  = 80-1;
+	NVIC_ST_CURRENT_R = 0;
+	NVIC_ST_CTRL_R    = 5;
+	while((NVIC_ST_CTRL_R&0x10000)==0){}
+	}
+	
+}
